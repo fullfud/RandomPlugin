86bef9f2c79475ead114f38762f280b493dd8133
fullfud <evildemonnonstop@gmail.com>
2025-06-17 22:07:32 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
+// build.gradle.kts
+
 plugins {
     id("java")
 }
 
-// ИЗМЕНЕНО: group теперь com.fullfud.randomlootchest
 group = "com.fullfud.randomlootchest"
 version = "1.0"
 
@@ -17,15 +18,10 @@ dependencies {
     compileOnly("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
 }
 
-tasks {
-    build {
-        dependsOn(jar)
-    }
-
-    jar {
-        archiveFileName.set("${rootProject.name}-${project.version}.jar")
-        from("src/main/resources") {
-            include("plugin.yml")
-        }
-    }
+// ИЗМЕНЕНО: Блок 'tasks' был значительно упрощен.
+// Мы убрали лишние инструкции, которые приводили к дублированию plugin.yml.
+// Теперь используется стандартное поведение Gradle, которое работает корректно.
+tasks.jar {
+    // Эта строка остается, чтобы у файла было красивое имя, например, RandomLootChest-1.0.jar
+    archiveFileName.set("${rootProject.name}-${project.version}.jar")
 }
